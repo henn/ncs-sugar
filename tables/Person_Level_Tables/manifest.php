fc6b4d03f7dd30bb0206633fdf485de657519447
jason-sd <jason-sd@sourceforge.net>
2011-02-23 01:09:33 -0600

    Update Person module date and version number

--- tables/Person_Level_Tables/manifest.php
+++ tables/Person_Level_Tables/manifest.php
@@ -51,9 +51,9 @@
           'icon' => '',
           'is_uninstallable' => true,
           'name' => 'project_Person_Level_Tables',
-          'published_date' => '2011-02-14 22:47:07',
+          'published_date' => '2011-02-22 19:47:07',
           'type' => 'module',
-          'version' => '1297723627',
+          'version' => '1299923627',
           'remove_tables' => 'prompt',
           );
 $installdefs = array (
