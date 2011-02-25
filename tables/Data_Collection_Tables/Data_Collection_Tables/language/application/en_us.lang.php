4f6568fc9c3e8aa5ee56ea1ec53c6d2f85014caa
michaelpcox <michaelpcox@gmail.com>
2011-02-25 12:14:46 -0800

    Revert "CAPS to mixed case change for Data Collection Table Labels"
    This reverts commit 6247bbbbd6f55afaf271959bae1f798335398a90.
    
    Conflicts:
    
    	tables/Data_Collection_Tables/Data_Collection_Tables/language/application/en_us.lang.php

---
+++ tables/Data_Collection_Tables/Data_Collection_Tables/language/application/en_us.lang.php
@@ -0,0 +1,61 @@
+<?php
+/*********************************************************************************
+ * SugarCRM is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2011 SugarCRM Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+
+
+$app_list_strings['moduleList']['NCSDC_Instrument'] = 'Instrument Information';
+$app_list_strings['moduleList']['NCSDC_Incident'] = 'Incident';
+$app_list_strings['moduleList']['NCSDC_CONTACT'] = NULL;
+$app_list_strings['moduleList']['NCSDC_EventInfo'] = 'Event Information';
+$app_list_strings['moduleList']['NCSDC_IncMedMultS'] = 'Incident Media Multi-Select';
+$app_list_strings['moduleList']['NCSDC_NIRNAccMltS'] = 'NIR No Access Multi-Select';
+$app_list_strings['moduleList']['NCSDC_IncUnatMltS'] = 'Incident Unanticipated Multi-Select';
+$app_list_strings['moduleList']['NCSDC_NIRRfsMltS'] = 'NIR Refusal Multi-Select';
+$app_list_strings['moduleList']['NCSDC_CntctInfo'] = 'Contact Information';
+$app_list_strings['moduleList']['NCSDC_INCIDENT'] = 'Incident';
+$app_list_strings['moduleList']['NCSDC_NonInterRpt'] = 'Non Interview Report';
+$app_list_strings['moduleList']['NCSDC_INSTRUMENT'] = 'Instrument Information';
+$app_list_strings['moduleList']['NCSDC_NIntRptVcnt'] = 'NIR Vacant Multi-Select';
+$app_list_strings['moduleList']['NCSDC_NIRDUTpMltS'] = 'NIR DU Type Multi Select';
+$app_list_strings['moduleList']['NCSDC_CntctLnk'] = 'Contact Linking';
+$app_list_strings['DU_NIR_REASON_CL1']['1'] = 'Vacant Dwelling Unit';
+$app_list_strings['DU_NIR_REASON_CL1']['2'] = 'Unable to Access';
+$app_list_strings['DU_NIR_REASON_CL1']['3'] = 'Demolished/Condemned Dwelling Unit';
+$app_list_strings['DU_NIR_REASON_CL1']['_5'] = 'Other';
+$app_list_strings['DU_NIR_REASON_CL1']['_7'] = 'Not Applicable';
+$app_list_strings['DU_NIR_REASON_CL1']['_4'] = 'Missing in Error';
+$app_list_strings['parent_type_display']['NCSDC_EventInfo'] = 'Event Information';
+$app_list_strings['record_type_display']['NCSDC_EventInfo'] = 'Event Information';
+$app_list_strings['record_type_display_notes']['NCSDC_EventInfo'] = 'Event Information';
