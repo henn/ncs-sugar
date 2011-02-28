0d2304424776de8aefad0e620527f90e7816d734
michaelpcox <michaelpcox@gmail.com>
2011-02-28 14:42:37 -0800

    Fix Module Data_Collection_Tables, there were no files. Reverted to BettyTest version.

---
+++ tables/Data_Collection_Tables/modules/NIRNAccMltS/vardefs.php
@@ -0,0 +1,101 @@
+<?php
+/*********************************************************************************
+ * SugarCRM is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2010 SugarCRM Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+$vardefs = array (
+  'fields' => 
+  array (
+    'nir_noaccess' => 
+    array (
+      'required' => false,
+      'name' => 'nir_noaccess',
+      'vname' => 'LBL_NIR_NOACCESS',
+      'type' => 'enum',
+      'massupdate' => 0,
+      'default' => 'Yes',
+      'comments' => '',
+      'help' => 'Description of lack of building access',
+      'importable' => 'true',
+      'duplicate_merge' => 'disabled',
+      'duplicate_merge_dom_value' => '0',
+      'audited' => true,
+      'reportable' => true,
+      'len' => 100,
+      'size' => '20',
+      'options' => 'NO_ACCESS_DESCR_CL1',
+      'studio' => 'visible',
+      'dependency' => false,
+    ),
+    'nir_noaccess_oth' => 
+    array (
+      'required' => false,
+      'name' => 'nir_noaccess_oth',
+      'vname' => 'LBL_NIR_NOACCESS_OTH',
+      'type' => 'varchar',
+      'massupdate' => 0,
+      'comments' => '',
+      'help' => 'Other Description of Lack of Access ',
+      'importable' => 'true',
+      'duplicate_merge' => 'disabled',
+      'duplicate_merge_dom_value' => '0',
+      'audited' => true,
+      'reportable' => true,
+      'len' => '255',
+      'size' => '20',
+    ),
+    'name' => 
+    array (
+      'name' => 'name',
+      'vname' => 'LBL_NAME',
+      'type' => 'name',
+      'dbType' => 'varchar',
+      'len' => '36',
+      'unified_search' => true,
+      'required' => true,
+      'importable' => 'required',
+      'massupdate' => 0,
+      'comments' => '',
+      'help' => '',
+      'duplicate_merge' => 'disabled',
+      'duplicate_merge_dom_value' => '0',
+      'audited' => true,
+      'reportable' => true,
+      'size' => '20',
+    ),
+  ),
+  'relationships' => 
+  array (
+  ),
+);
+?>
---
+++ tables/Data_Collection_Tables/modules/NonInterRpt/language/en_us.lang.php
@@ -0,0 +1,102 @@
+<?php
+/*********************************************************************************
+ * SugarCRM is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2010 SugarCRM Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+$mod_strings = array (
+  'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+  'LBL_ASSIGNED_TO_NAME' => 'User',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Date Created',
+  'LBL_DATE_MODIFIED' => 'Date Modified',
+  'LBL_MODIFIED' => 'Modified By',
+  'LBL_MODIFIED_ID' => 'Modified By Id',
+  'LBL_MODIFIED_NAME' => 'Modified By Name',
+  'LBL_CREATED' => 'Created By',
+  'LBL_CREATED_ID' => 'Created By Id',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_DELETED' => 'Deleted',
+  'LBL_NAME' => 'Non Interview Report ID',
+  'LBL_CREATED_USER' => 'Created by User',
+  'LBL_MODIFIED_USER' => 'Modified by User',
+  'LBL_LIST_NAME' => 'Name',
+  'LBL_LIST_FORM_TITLE' => 'Non Interview Report List',
+  'LBL_MODULE_NAME' => 'Non Interview Report',
+  'LBL_MODULE_TITLE' => 'Non Interview Report',
+  'LBL_HOMEPAGE_TITLE' => 'My Non Interview Report',
+  'LNK_NEW_RECORD' => 'Create Non Interview Report',
+  'LNK_LIST' => 'View Non Interview Report',
+  'LNK_IMPORT_NCSDC_NON_INTERVIEW_REPRT' => 'Import Non Interview Report',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Non Interview Report',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+  'LBL_NCSDC_NON_INTERVIEW_REPRT_SUBPANEL_TITLE' => 'Non Interview Report',
+  'LBL_NEW_FORM_TITLE' => 'New Non Interview Report',
+  'LBL_NIR_ID' => 'Non Interview Report ID',
+  'LBL_NIR' => 'Non Interview Report',
+  'LBL_NIR_VAC_INFO' => 'Non Interview Report Vacancy Info',
+  'LBL_NIR_VAC_INFO_OTH' => ' Non Interview Report Vacancy Info Other',
+  'LBL_NIR_NOACCESS' => 'Non Interview Report Noaccess',
+  'LBL_NIR_NOACCESS_OTH' => 'Non Interview Report No Access Other',
+  'LBL_NIR_ACCESS_ATTEMPT' => 'Non Interview Report Access Attempt',
+  'LBL_NIR_ACCESS_ATTEMPT_OTH' => 'Non Interview Report Access Attempt Other',
+  'LBL_NIR_TYPE_PERSON' => 'Non Interview Report Type Person',
+  'LBL_NIR_TYPE_PERSON_OTH' => 'Non Interview Report Type Person Other',
+  'LBL_COG_INFORM_RELATION' => 'COG Inform Relation',
+  'LBL_COG_INFORM_RELATION_OTH' => 'COG Inform Relation Other',
+  'LBL_COG_DIS_DESC' => 'Cognitive Disability Description',
+  'LBL_PERM_DISABILITY' => 'Permanent Disability',
+  'LBL_DECEASED_INFORM_RELATION' => 'Deceases Inform Relation',
+  'LBL_DECEASED_INFORM_OTH' => 'Deceased Inform Other',
+  'LBL_YOD' => 'Year of Death',
+  'LBL_WHO_REFUSED' => 'Who Refused',
+  'LBL_WHO_REFUSED_OTH' => 'Who Refused Other',
+  'LBL_REFUSER_STRENGTH' => 'Refuser Strength',
+  'LBL_REF_ACTION' => 'Reference Action',
+  'LBL_LT_ILLNESS_DESC' => 'Longterm Illness Description',
+  'LBL_PERM_LTR' => 'Permanent LTR',
+  'LBL_REASON_UNAVAIL' => 'Reason Unavailable',
+  'LBL_REASON_UNAVAIL_OTH' => 'Reason Unavailable Other',
+  'LBL_DATE_AVAILABLE' => 'Date Available',
+  'LBL_DATE_MOVED' => 'Date Moved',
+  'LBL_MOVED_LENGTH_TIME' => 'Moved Lenght Time',
+  'LBL_MOVED_UNIT' => 'Moved Unit',
+  'LBL_MOVED_RELATION_OTH' => 'Moved Relation Other',
+  'LBL_MOVED_INFORM_RELATION' => 'Moved Inform Relation',
+  'LBL_NIR_OTHER' => 'Non Interview Report Other',
+  'LBL_NON_INTERVIEW_RPT' => 'Non Interview Report',
+  'LBL_STATE_DEATH' => 'State Death',
+  'LNK_IMPORT_NCSDC_NONINTERRPT' => 'Import Non Interview Report',
+  'LBL_NCSDC_NONINTERRPT_SUBPANEL_TITLE' => 'Non Interview Report',
+);
+?>
