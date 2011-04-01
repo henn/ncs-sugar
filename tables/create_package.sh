30dd46812726b17df7306e3efcac0552b4421637
jason-sd <jason-sd@sourceforge.net>
2011-04-01 02:07:48 -0500

    Introduce super-package generation
    Invoked by calling create_package.sh with "-c", the combined package (currently
    output/combined.zip) will only need to be installed once to install all of the
    regular NCS Sugar tables instead of needing to install all 7 manually. This is
    intended to save developer time.
    
    Individual packages (Person-level, Geographic, etc) will still need to be
    deployed as desired through the module builder. A future -d option may be introduced to do this.
    
    Please note that IDCreation has not yet been integrated, so that module will
    still need to be installed separately.

--- tables/create_package.sh
+++ tables/create_package.sh
@@ -3,6 +3,11 @@
 # Unix script to create a packaging for every module
 # Depends on having a "zip" binary compatible with the InfoZIP variety
 # on Linux
+# Pass "-c" to create a single SugarCRM module containing all
+# of the NCS modules (useful for developers)
+
+# TODO: Add a "-d" (for deploy) option to, rather than sending the modules to
+# the module builder, deploy them directly without editability
 
 # Where the Sugar modules are located.
 # Format should be: $DIRNAME/{manifest.php,$DIRNAME, ...}
@@ -11,6 +16,7 @@ MODULE_DIRS="Locating_Tracing_Tables Specific_Code_Tables\
 	Geographic_Tables Person_Level_Tables IDCreation"
 
 DESTDIR="output"
+HELPER_DIR="templates"
 
 FINAL_FILENAME="`date +'%F'`-ncs-modules-snapshot"
 
@@ -23,6 +29,9 @@ fi
 rm -rf $DESTDIR
 mkdir $DESTDIR
 
+####### Separate packaging
+
+# Create the individual sets of modules
 for i in $MODULE_DIRS
 do
 	cd $i
@@ -36,6 +45,7 @@ cd $DESTDIR
 
 zip -9q ${FINAL_FILENAME}.zip *.zip
 
+
 if [ $? -eq 0 ]; then
 	echo "Successfully created ${FINAL_FILENAME}.zip. You can now release this file by running:"
 	echo "   cp $DESTDIR/${FINAL_FILENAME}.zip \"/home/frs/project/n/nc/ncs-sugar/Pre-release modules/\"" 
@@ -43,3 +53,49 @@ else
 	echo "Unable to create ${FINAL_FILENAME}.zip"
 	exit 1
 fi
+
+####### Combined packaging
+
+# Only do this if requested via the "-c" arg for combined
+if [ $# -lt 1 -o "$1" != '-c' ]; then
+	exit 0
+fi
+
+# Remove IDCreation for now
+MODULE_DIRS="`echo $MODULE_DIRS | sed 's/IDCreation//'`"
+
+# Create the super-module, which can be installed instead of the indivual ones
+ZIP_ARGS=''
+COUNT=0
+
+# Directory where things are combined
+COMBINED="combined"
+mkdir $COMBINED
+
+#Assuming we are already in $DESTDIR
+cd $COMBINED
+
+#TODO: Update version and published dates in the manifest(s)
+cp ../../$HELPER_DIR/combined_manifest1.php manifest.php
+
+for i in $MODULE_DIRS
+do
+	echo "$COUNT => array ('from'=>'<basepath>/$i','to'=>'custom/modulebuilder/packages/$i')," >> manifest.php
+	pushd  ../../$i >> /dev/null
+	zip -r9q ../$DESTDIR/$COMBINED.zip $i
+	popd >> /dev/null
+	COUNT=`expr $COUNT + 1`
+done
+
+# Need to append the last few lines onto the manifest
+cat ../../$HELPER_DIR/combined_manifest2.php >> manifest.php
+zip -r9qj ../$COMBINED.zip manifest.php
+
+cd ..
+
+# Copy other files
+zip -r9qj $COMBINED.zip ../$HELPER_DIR/{README,LICENSE}.txt
+
+
+echo "Created combined package (minus IDCreation...to come later) as: output/$COMBINED.zip"
+
---
+++ tables/templates/combined_manifest1.php
@@ -0,0 +1,63 @@
+    <?php
+/*********************************************************************************
+ * SugarCRM is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2011 SugarCRM Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+
+
+    $manifest = array (
+         'acceptable_sugar_versions' => 
+          array (
+            '6.1.2'
+          ),
+          'acceptable_sugar_flavors' =>
+          array(
+            'CE', 'PRO','ENT'
+          ),
+          'readme'=>'',
+          'key'=>'NCS',
+          'author' => 'NCS Sugar Consortium',
+          'description' => 'Collection of SugarCRM modules to provide National Children\'s Study informatics
+',
+          'icon' => '',
+          'is_uninstallable' => true,
+          'name' => 'Collated_NCS_Modules',
+          'published_date' => '2011-03-31 21:19:24',
+          'type' => 'module',
+          'version' => '1399999999',
+          'remove_tables' => 'prompt',
+          );
+$installdefs = array (
+  'id' => 'Collated_NCS_Modules',
+  'copy' => 
+  array (
