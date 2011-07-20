d720803d68fa32fa6764c27b51595881a420ce72
michaelpcox <michaelpcox@gmail.com>
2011-07-20 12:25:14 -0700

    IDCreation created for these modules which were found to be not generating IDs.  T_MSOutEval, ST_MSOutLang2, ST_MSOutRace, ST_MSOutTarg.

---
+++ tables/Custom_Resources/IDCreation_resources/modules/ST_MSOutLang2/views/view.edit.php
@@ -0,0 +1,23 @@
+<?php 
+
+	if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+	require_once('include/MVC/View/views/view.edit.php');
+	require_once 'include/utils.php';
+
+	class ST_MSOutLang2ViewEdit extends ViewEdit {
+		
+		function PLT_PersonViewEdit(){
+			parent::ViewEdit();
+		}
+
+		function display() {
+			require_once 'ncs_framework/ncs_controller.php';
+			$ncs = new NCS($this->bean);
+			$ncs->identifier = 'OL';  //need to be changed later on.
+			$this->ss->assign("NAME", $ncs->get_name());
+			parent::display();
+		}				
+	} 
+
+?>
