3bebe9d19ac0665857aacade94b6b5420dbdd881
michaelpcox <michaelpcox@gmail.com>
2011-07-22 16:06:17 -0700

    Accidently deleted this file!  Commit containing bad file was 9e26fa1ffedd5fe80ccf6aacc49562de96da691d reverted to en_us.lang.php from 77e6ec4bf1fa29286dbb05e5750a864d10714da3

--- tables/Custom_Resources/Activities_resources/SugarModules/modules/Notes/language/en_us.lang.php
+++ tables/Custom_Resources/Activities_resources/SugarModules/modules/Notes/language/en_us.lang.php
@@ -1,5 +1,27 @@
 <?php 
- // created: 2011-07-20 18:05:18
-$mod_strings['LBL_CONTACT_NAME'] = 'Contact ID ';
+ // created: 2011-06-22 13:36:08
+$mod_strings['LBL_DATE_TIME'] = 'Contact Start Date & Time:';
+$mod_strings['LBL_START_DATE_TIME'] = 'Contact Start Date & Time:';
+$mod_strings['LBL_END_DATE_TIME'] = 'Contact End Date & Time';
+$mod_strings['LBL_CONTACT_TYPE'] = 'Contact Type';
+$mod_strings['LBL_CONTACT_TYPE_OTHER'] = 'Other Contact Type';
+$mod_strings['LBL_CONTACT_LANGUAGE'] = 'Contact Language';
+$mod_strings['LBL_CONTACT_LANG_OTH'] = 'Other language of interview';
+$mod_strings['LBL_CONTACT_INTERPRET'] = 'Who translated questionnaire during contact?  ';
+$mod_strings['LBL_CONTACT_INTERPRET_OTH'] = 'Other method of interpretation';
+$mod_strings['LBL_CONTACT_LOCATION'] = 'Location of Contact';
+$mod_strings['LBL_CONTACT_LOCATION_OTH'] = 'Other location of contact ';
+$mod_strings['LBL_CONTACT_PRIVATE'] = 'Were there privacy issues during the contact?  ';
+$mod_strings['LBL_CONTACT_PRIVATE_DETAIL'] = 'Description of privacy issues during field contact';
+$mod_strings['LBL_CONTACT_DISTANCE'] = 'Total miles staff member traveled for contact';
+$mod_strings['LBL_WHO_CONTACTED'] = 'Person who provided information associated with contact';
+$mod_strings['LBL_WHO_CONTACT_OTH'] = 'Other person providing information';
+$mod_strings['LBL_DESCRIPTION'] = 'Contact Comment:';
+$mod_strings['LBL_CONTACT_DISP'] = 'Contact Disposition';
+$mod_strings['LBL_CONTACT_NAME'] = 'Contact ID: ';
+$mod_strings['LBL_NOTE_SUBJECT'] = 'Contact ID:';
+$mod_strings['LBL_EVENT'] = 'Event';
+$mod_strings['LBL_LIST_SUBJECT'] = 'Contact ID:';
+$mod_strings['LBL_NOTE_STATUS'] = 'Comments:';
 
 ?>
