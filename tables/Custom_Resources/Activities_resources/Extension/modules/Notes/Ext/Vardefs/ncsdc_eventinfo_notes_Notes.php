97b6bf742063cf99367e73ce2ad2394d8c1f6fb2
michaelpcox <michaelpcox@gmail.com>
2011-10-13 14:39:19 -0700

    From Johnny:
    I've found what caused the relationships problem in notes and meetings modules and fixed them except this error
    
    Thu Oct 13 16:09:40 2011 [912][1][FATAL] Missing relationship definition meetings_olt_provider. skipping meetings_olt_provider subpanel

--- tables/Custom_Resources/Activities_resources/Extension/modules/Notes/Ext/Vardefs/ncsdc_eventinfo_notes_Notes.php
+++ tables/Custom_Resources/Activities_resources/Extension/modules/Notes/Ext/Vardefs/ncsdc_eventinfo_notes_Notes.php
@@ -3,7 +3,7 @@
 $dictionary["Note"]["fields"]["ncsdc_eventinfo_notes"] = array (
   'name' => 'ncsdc_eventinfo_notes',
   'type' => 'link',
-  'relationship' => 'ncsdc_eventinfo_notes',
+  'relationship' => 'ncsdc_eventinfo_activities_notes',
   'source' => 'non-db',
   'vname' => 'LBL_NCSDC_EVENTINFO_NOTES_FROM_NCSDC_EVENTINFO_TITLE',
 );
@@ -14,7 +14,7 @@ $dictionary["Note"]["fields"]["ncsdc_eventnfo_notes_name"] = array (
   'vname' => 'LBL_NCSDC_EVENTINFO_NOTES_FROM_NCSDC_EVENTINFO_TITLE',
   'save' => true,
   'id_name' => 'ncsdc_even18c0entinfo_ida',
-  'link' => 'ncsdc_eventinfo_notes',
+  'link' => 'ncsdc_eventinfo_activities_notes',
   'table' => 'ncsdc_eventinfo',
   'module' => 'NCSDC_EventInfo',
   'rname' => 'name',
@@ -22,7 +22,7 @@ $dictionary["Note"]["fields"]["ncsdc_eventnfo_notes_name"] = array (
 $dictionary["Note"]["fields"]["ncsdc_even18c0entinfo_ida"] = array (
   'name' => 'ncsdc_even18c0entinfo_ida',
   'type' => 'link',
-  'relationship' => 'ncsdc_eventinfo_notes',
+  'relationship' => 'ncsdc_eventinfo_activities_notes',
   'source' => 'non-db',
   'reportable' => false,
   'side' => 'right',
