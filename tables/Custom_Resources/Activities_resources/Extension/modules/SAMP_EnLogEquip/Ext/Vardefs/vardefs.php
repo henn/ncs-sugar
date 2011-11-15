c48aac308ecb6ba2e092282c4485dc77acf53fe5
michaelpcox <michaelpcox@gmail.com>
2011-11-15 13:10:12 -0800

    Fix to SCTABLES mistake (didnt catch len=100 and that caused problems) revised to len=50

--- tables/Custom_Resources/Activities_resources/Extension/modules/SAMP_EnLogEquip/Ext/Vardefs/vardefs.php
+++ tables/Custom_Resources/Activities_resources/Extension/modules/SAMP_EnLogEquip/Ext/Vardefs/vardefs.php
@@ -4,6 +4,6 @@ $dictionary['SAMP_EnLogEquip']['unified_search_default_enabled'] = true;
 $dictionary['SAMP_EnLogEquip']['fields']['name']['unified_search'] = true;
 $dictionary['SAMP_EnLogEquip']['fields']['samp_enlogep_enequip_name']['required']=true;
 $dictionary['SAMP_EnLogEquip']['fields']['samp_enloge_srscinfo_name']['required']=true;
-$dictionary['SAMP_EnLogEquip']['fields']['samp_enlogequip_st_staffrstr']['required']=true;
+$dictionary['SAMP_EnLogEquip']['fields']['samp_enlogestaffrstr_name']['required']=true;
 ?>
 
