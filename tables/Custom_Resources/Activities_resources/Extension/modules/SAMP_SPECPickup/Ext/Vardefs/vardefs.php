38864508c99d8b803443567cc5e3764335086511
michaelpcox <michaelpcox@gmail.com>
2011-11-23 10:33:16 -0800

    From Anna: November 22, 2011 9:35 AM I am sending you one fix I did to the SPECPickup. I was asked to change the Instrument ID not to be required.

--- tables/Custom_Resources/Activities_resources/Extension/modules/SAMP_SPECPickup/Ext/Vardefs/vardefs.php
+++ tables/Custom_Resources/Activities_resources/Extension/modules/SAMP_SPECPickup/Ext/Vardefs/vardefs.php
@@ -3,7 +3,7 @@ $dictionary['SAMP_SPECPickup']['unified_search'] = true;
 $dictionary['SAMP_SPECPickup']['unified_search_default_enabled'] = true;
 $dictionary['SAMP_SPECPickup']['fields']['name']['unified_search'] = true;
 $dictionary['SAMP_SPECPickup']['fields']['samp_specpieventinfo_name']['required']=true;
-$dictionary['SAMP_SPECPickup']['fields']['samp_specpinstrument_name']['required']=true;
+//$dictionary['SAMP_SPECPickup']['fields']['samp_specpinstrument_name']['required']=true;
 $dictionary['SAMP_SPECPickup']['fields']['samp_specpistaffrstr_name']['required']=true;
 $dictionary['SAMP_SPECPickup']['fields']['samp_specpi_spscinfo_name']['required']=true;
 ?>
